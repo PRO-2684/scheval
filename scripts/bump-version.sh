6a1e79e980471abd4a3e1f463b97b857417d3891
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-01-02 19:07:08 +0800

    Bump version to 0.1.1

---
+++ scripts/bump-version.sh
@@ -0,0 +1,22 @@
+# Get the current version
+VERSION=$(rg --no-filename '^version = "(.*)"' Cargo.toml --replace '$1')
+
+# Increment the patch version
+MAJOR=$(echo $VERSION | cut -d. -f1)
+MINOR=$(echo $VERSION | cut -d. -f2)
+PATCH=$(echo $VERSION | cut -d. -f3)
+PATCH=$((PATCH + 1))
+VERSION="$MAJOR.$MINOR.$PATCH"
+
+# Update the version in Cargo.toml
+sed -i "s/^version = \".*\"/version = \"$VERSION\"/" Cargo.toml
+
+# Commit the change
+git add Cargo.toml
+git commit -m "Bump version to v$VERSION"
+
+# Tag
+git tag -a "$VERSION" -m "Version v$VERSION"
+
+# Display
+echo "Bump version to v$VERSION"
